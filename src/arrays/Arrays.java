2783fcea3f58ec64329f870dd8e3ee3f11ea5b1d
dam108 <dam108@A20E926.wirtz.local>
2019-11-08 17:16:17 +0100

    Desde casa 08/11/2019

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        
+        System.out.println("Hello arrays");
         
     }   // fin main
     
