04a8781f7f6ab1ae6fab57902ccee6b6f5897463
dam108 <dam108@A20E926.wirtz.local>
2019-11-08 09:47:51 +0100

    desde clase 08/11/2019

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arrays;
-
-/**
- *
- * @author dam108
- */
+import java.util.Scanner;
 public class Arrays {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hello arrays 2");
-    }
+        Scanner teclado = new Scanner(System.in);
+        
+        
+    }   // fin main
     
-}
+    
+}       // fin class
